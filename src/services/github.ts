2fd8650203a8aeba85ca0809b66019877a00f230
João Felipe da Silva Santos <joao.santos@cadastra.com>
2025-10-26 23:31:49 -0300

    feat: add github service to fetchUser details

---
+++ src/services/github.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const GITHUB_BASE = "https://api.github.com";
+
+const token = import.meta.env.VITE_GITHUB_TOKEN;
+
+const api = axios.create({
+  baseURL: GITHUB_BASE,
+  headers: token ? { Authorization: `token ${token}` } : undefined,
+});
+
+
+export const fetchUser = async (username: string) => {
+  const { data } = await api.get(`/users/${username}`);
+  return data;
+};
