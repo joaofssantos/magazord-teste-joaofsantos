b5b0d0ec9649adc20859b05013e904bce14285db
João Felipe da Silva Santos <joao.santos@cadastra.com>
2025-10-30 12:22:50 -0300

    fix: adjust config, change commands on package and fix tailwind on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -37,11 +37,11 @@ export const App = () => {
   return (
     <div className="min-h-screen bg-white">
       <Header onLogoClick={goHome} />
-      <main className="text-center">
-        <h1 className="font-bold mb-4">Buscar perfil do GitHub</h1>
+      <main className="text-center w-11/12 mx-auto">
+        <h1 className="font-bold my-8">Buscar perfil do GitHub</h1>
         <p className="mb-6 text-short text-light-dark">Digite o nome de usuário na URL ou use o campo abaixo para abrir /{`{username}`}</p>
         <div className="flex gap-2 justify-center">
-          <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Insira o usuario do Github" className=" border" />
+          <input value={input} onChange={(e) => setInput(e.target.value)} placeholder="Insira o usuario do Github" className=" border w-[320px] px-3" />
           <button onClick={() => goTo(input)} className="px-4 py-2 bg-gradient text-white rounded">Buscar</button>
         </div>
       </main>
