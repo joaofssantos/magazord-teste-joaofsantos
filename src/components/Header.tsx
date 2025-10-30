b41796bb91e32a31016a17992cc0e2c4514edeb8
João Felipe da Silva Santos <joao.santos@cadastra.com>
2025-10-30 00:54:42 -0300

    feat: finish header, add eslint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,30 @@
-import React from 'react'
+
 import GithubLogo from "../assets/Github.svg"
-const Header: React.FC = () => {
+const Header = () => {
   return (
     <header className="w-full">
-
       <div className="w-full bg-primary border-b">
         <div className="max-w-[1280px] mx-auto px-4">
           <div className="h-[72px] flex items-center">
-
-            <a href="#" className="flex items-center gap-3 text-white font-semibold">
+            <a href="/" className="flex items-center gap-3 text-white font-semibold">
               <img src={GithubLogo} alt="GitHub Logo" className="w-24 h-8" />
             </a>
-
-
             <nav className="ml-4 flex items-center h-full">
               <ul className="flex items-center gap-2 text-base text-white">
                 <li className="px-2 text-[1rem] font-light text-white hover:text-light-dark cursor-pointer">/</li>
-                <li className="px-2 text-[1rem] font-light text-white hover:text-light-dark cursor-pointer">Profile</li>
+                <li className="px-2 text-[1rem] font-light text-white hover:text-light-dark cursor-pointer">
+                     <a href="/" className="text-white hover:text-light-dark hover:no-underline">
+                  Profile
+                  </a>
+                  </li>
               </ul>
             </nav>
           </div>
         </div>
       </div>
-
-   
     </header>
   )
 }
 
 export default Header
+
